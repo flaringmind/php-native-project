b077e61041b929c3b3c83b2063b90667fc38eb98
flaringmind <andlukyanow@gmail.com>
2024-10-15 13:45:11 +0300

    Lesson 2.5 completed

--- src/public/index.php
+++ src/public/index.php
@@ -2,15 +2,26 @@
 
 declare(strict_types=1);
 
-require_once '../PaymentGateway/Stripe/Transaction.php';
-require_once '../PaymentGateway/Paddle/Transaction.php';
-require_once '../PaymentGateway/Paddle/CustomerProfile.php';
+/*
+require_once '../app/PaymentGateway/Stripe/Transaction.php';
+require_once '../app/PaymentGateway/Paddle/Transaction.php';
+require_once '../app/PaymentGateway/Paddle/CustomerProfile.php';
+spl_autoload_register(function ($class) {
+    $path = __DIR__ . '/../' . lcfirst(str_replace('\\', '/', $class)) . '.php';
+    if (file_exists($path)) {
+        require $path;
+    }
+});
+*/
 
-use PaymentGateway\Paddle\{Transaction, CustomerProfile};
-use PaymentGateway\Stripe\Transaction as StripeTransaction;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\PaymentGateway\Paddle\Transaction;
+
+$uid = new \Ramsey\Uuid\UuidFactory();
+
+echo $uid->uuid4();
 
 $paddleTransaction = new Transaction();
-$stripeTransaction = new StripeTransaction();
-$paddleCustomerProfile = new CustomerProfile();
-
-var_dump($paddleTransaction, $stripeTransaction);
+var_dump($paddleTransaction);
