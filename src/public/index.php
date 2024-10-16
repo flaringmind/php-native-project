4f1fe0e080f652ecf369a69fad8b085e9d74547c
flaringmind <andlukyanow@gmail.com>
2024-10-17 01:07:42 +0300

    Lesson 2.13 completed

--- src/public/index.php
+++ src/public/index.php
@@ -2,6 +2,13 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$invoice = new \App\Invoice();
-var_dump($invoice);
+$classA = new \App\ClassA();
+$classB = new \App\ClassB();
 
+//echo $classA->getName(). '<br />';
+//echo $classB->getName(). '<br />';
+
+//echo \App\ClassA::getName(). '<br />';
+//echo \App\ClassB::getName(). '<br />';
+
+var_dump(\app\ClassB::make());
