46f6cf1094ca18e3ac5a25b890ccdfbd3f589ad2
flaringmind <andlukyanow@gmail.com>
2024-10-17 19:18:58 +0300

    Lesson 2.16 completed

--- src/public/index.php
+++ src/public/index.php
@@ -5,5 +5,8 @@ use \App\ClassA;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $obj = new ClassA(20, 50);
+$obj2 = new ClassA(20, 50);
+$obj3 = $obj2;
+var_dump($obj2 == $obj3);
+var_dump($obj2 === $obj3);
 
-var_dump($obj->bar());
