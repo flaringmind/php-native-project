74c2744b62b845469754009bebefd9e9f5fc7346
flaringmind <andlukyanow@gmail.com>
2024-10-20 15:11:27 +0300

    Lesson 2.21 completed

--- src/public/index.php
+++ src/public/index.php
@@ -4,26 +4,7 @@ use \App\Invoice;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-set_exception_handler(function (\Throwable $e) {
-    var_dump($e->getMessage());
-});
+$dateTime = new DateTime('yesterday noon', new DateTimeZone('Europe/Amsterdam'));
 
-echo array_rand([], 1);
-
-var_dump(processInvoice());
-
-function processInvoice() {
-    $invoice = new Invoice(new \App\Customer([1]));
-
-    try {
-        $invoice->process(-24);
-        return true;
-    } catch (\App\Exception\MissingBillingInfoException | \InvalidArgumentException $e) {
-        echo $e->getMessage() . $e->getFile() . ': on line ' . $e->getLine() . '<br />';
-        return false;
-    } finally {
-        echo 'finally block <br />';
-        return 'Finally';
-    }
-}
+var_dump($dateTime);
 
